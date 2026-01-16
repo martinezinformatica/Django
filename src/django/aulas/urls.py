a51126432dc0d3a4c61f00dc6807b566ad3697a8
martinezInformatica <martinezinformatica16@gmail.com>
2026-01-15 21:12:11 -0300

    urls parametros

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hello
+from .views import hello, bye, edad
 
 urlpatterns = [
     path('hello/', hello ),
+    path('bye/', bye ),
+    path('edad/<int:anios>/<int:futuro>', edad ),
 ]
--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,8 +1,20 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 def hello(request): 
     return HttpResponse("hola mundo")
+
+def bye(request): 
+    return HttpResponse("chau mundo")
+
+def edad(request, anios, futuro):
+
+    incremento = futuro - date.today().year
+    cumplira = anios + incremento
+    mensaje = "En el año %d  cumpliras %d años" %(futuro, cumplira) 
+
+    return HttpResponse(mensaje)
 
 
 
