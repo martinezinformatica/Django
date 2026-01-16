4f6c94c174d8ae96543511a54f88a8805f97e201
martinezInformatica <martinezinformatica16@gmail.com>
2026-01-16 19:52:10 -0300

    Templates

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import hello, bye, edad
+from .views import hello, bye, edad, primer_plantilla, segunda_plantilla, tercer_plantilla, cuarta_plantilla
 
 urlpatterns = [
     path('hello/', hello ),
     path('bye/', bye ),
     path('edad/<int:anios>/<int:futuro>', edad ),
+    path('plantilla1/', primer_plantilla ),
+    path('plantilla2/', segunda_plantilla ),
+    path('plantilla3/', tercer_plantilla ),
+    path('plantilla4/', cuarta_plantilla ),
 ]
--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,6 +1,9 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
 
 def hello(request): 
     return HttpResponse("hola mundo")
@@ -15,6 +18,67 @@ def edad(request, anios, futuro):
     mensaje = "En el año %d  cumpliras %d años" %(futuro, cumplira) 
 
     return HttpResponse(mensaje)
+
+def primer_plantilla(request):
+    plantilla = """
+    <htnl>
+      <body>
+        <h2> 
+          Hola {{nombre}} estas es mi primer plantilla 
+        </h2>
+      </body>
+    </html>
+"""
+
+    tpl = Template(plantilla)
+    ctx = Context({
+        "nombre": "Julio Martinez"
+    })
+    mensaje = tpl.render(ctx)
+
+    return HttpResponse(mensaje)
+
+def segunda_plantilla(request):
+    
+
+    tpl = get_template("segunda_plantilla.html")
+
+    mensaje = tpl.render({
+        
+        "nombre": "Julio Martinez",
+        "fecha_actual": date.today()
+    })
+    return HttpResponse(mensaje)
+
+def tercer_plantilla(request):
+    
+   return render(request,"tercer_plantilla.html",{
+      "nombre": "Julio Martinez",
+        "fecha_actual": date.today()
+      
+   })
+
+class Empleado(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+        
+
+def cuarta_plantilla(request):
+   
+   empleado = Empleado("Julio","Martinez")
+
+   laborables = ["lunes","martes","miercoles", "jueves"]
+    
+   return render(request,"cuarta_plantilla.html",{
+      "mi_empleado": empleado,
+        "fecha_actual": date.today(),
+        "dias_laborables": laborables
+      
+   })
+
+
+
 
 
 
