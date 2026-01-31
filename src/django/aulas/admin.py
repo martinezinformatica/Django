cb6d3419d136eb4169636c6c49b299df1363e78e
martinezInformatica <martinezinformatica16@gmail.com>
2026-01-31 11:59:19 -0300

    Postgres

--- src/django/aulas/admin.py
+++ src/django/aulas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from aulas.models import Musician, Person, Album
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Album)
+admin.site.register(Musician)
